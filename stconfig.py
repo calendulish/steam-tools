c2c604d03057341222dca2b8f0114d54379b9d3c
Lara Maia <dev@lara.click>
2015-12-10 11:11:22 -0200

    If config dir doesn't exists, create it.

--- stconfig.py
+++ stconfig.py
@@ -12,8 +12,8 @@ def init(fileName):
     xdg_dir = os.getenv('XDG_CONFIG_HOME', os.path.join(os.path.expanduser('~'), '.config'))
     config_file = os.path.join(xdg_dir, 'steam-tools', fileName)
 
-    #if not os.path.isdir(os.path.dirname(config_file)):
-    #    os.mkdir(os.path.dirname(config_file)
+    if not os.path.isdir(os.path.dirname(config_file)):
+        os.mkdir(os.path.dirname(config_file))
 
     if os.path.isfile(config_file):
         config.read(config_file)
--- stlogger.py
+++ stlogger.py
@@ -6,11 +6,15 @@ import logging
 
 def init(fileName):
     xdg_dir = os.getenv('XDG_CONFIG_HOME', os.path.join(os.path.expanduser('~'), '.config'))
+    path = os.path.join('xdg_dir', 'steam-tools')
+
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
     logger = logging.getLogger("root")
     logger.setLevel(logging.DEBUG)
 
-    file = logging.FileHandler(os.path.join(xdg_dir, 'steam-tools', fileName))
+    file = logging.FileHandler(os.path.join(path, fileName))
     file.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
     file.setLevel(logging.DEBUG)
     logger.addHandler(file)
@@ -20,7 +24,7 @@ def init(fileName):
     console.setLevel(logging.INFO)
     logger.addHandler(console)
 
-    httpfile = logging.FileHandler(os.path.join(xdg_dir, 'steam-tools', 'requests_'+fileName))
+    httpfile = logging.FileHandler(os.path.join(path, 'requests_'+fileName))
     httpfile.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
     httpfile.setLevel(logging.DEBUG)
 
