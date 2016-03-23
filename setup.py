ed48d7f595e4a41282eaa7c8a337c4234df10f49
Lara Maia <dev@lara.click>
2016-03-23 10:41:34 -0300

    [setup.py] Add py2exe options only on Windows

--- setup.py
+++ setup.py
@@ -8,19 +8,23 @@ if os.name == 'nt':
 data_files=[('lib64', ['lib64/libsteam_api.dll']),
             ('lib32', ['lib32/libsteam_api.dll']),
             ]
-            
+
 console_programs=['fake-steam-app.py',
                   'steam-card-farming.py',
                   'steamgifts-bump.py',
                   'steamgifts-join.py',
                     ]
 
-py2exe_options = {'py2exe': {
-                          'bundle_files': 3,
-                          'optimize': 1,
-                          'compressed': 0,
-                          },
-                }
+def py2exe_options():
+    if os.name == 'nt':
+        options = {'py2exe': {'bundle_files': 3,
+                              'optimize': 1,
+                              'compressed': 0,},}
+
+        return {'console': console_programs,
+                'options': options,}
+    else:
+        return {}
 
 setup(
     name='Steam Tools',
@@ -29,7 +33,6 @@ setup(
     author='Lara Maia',
     author_email='dev@lara.click',
     url='http://github.com/ShyPixie/steam-tools',
-    options = py2exe_options,
     data_files=data_files,
-    console=console_programs,
+    **py2exe_options()
     )
