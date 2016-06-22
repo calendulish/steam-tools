4bc3c950d88184c9814e41691db735ee0f773173
Lara Maia <dev@lara.click>
2016-06-22 16:43:21 -0300

    [GUI] Add stfakeapp

--- stlib/stconsole.py
+++ stlib/stconsole.py
@@ -17,71 +17,39 @@
 #
 
 import sys
-import os
-import locale
-from signal import signal, SIGINT
-from subprocess import Popen, PIPE
 
 from stlib import stlogger
+from stlib.stfakeapp import STFakeApp
 
-if os.name is 'posix':
-    import site
 
 class STConsole:
     def __init__(self, cParams):
-        signal(SIGINT, self.safe_exit)
         self.logger = stlogger.getLogger('VERBOSE')
         self.program = cParams.cli[0]
         self.cParams = cParams
 
         if self.program in dir(self):
-            eval('self.'+self.program+'()')
-            self.safe_exit()
+            eval('self.' + self.program + '()')
         else:
-            self.logger.critical("Please, check your command line.")
-            self.logger.critical("The program %s don't exist", self.program)
-
-    def safe_exit(self, SIG=None, FRM=None):
-        if 'fake_app' in dir(self):
-            self.fake_app.terminate()
-            self.logger.debug("Waiting to fakeapp terminate.")
-            self.fake_app.wait()
-            if self.fake_app.returncode and 'fake_app_stderr' in dir(self):
-                error = self.fake_app_stderr.decode(locale.getpreferredencoding())
-                self.logger.critical(error)
-                return 1
-
-    def find_fake_app(self):
-        paths = []
-        paths.append(os.path.dirname(os.path.abspath(sys.argv[0])))
-        paths.append(os.path.join(paths[0], 'stlib'))
-
-        if os.name is 'posix':
-            paths.append(os.path.join(site.getsitepackages()[1], 'stlib'))
-
-        for path in paths:
-            for ext in [ '', '.exe', '.py' ]:
-                full_path = os.path.join(path, 'fakeapp'+ext)
-
-                if os.path.isfile(full_path):
-                    return full_path
-
-        self.logger.critical("I cannot found some libraries needed.")
-        self.logger.critical("Please, verify your installation.")
-        sys.exit(1)
+            self.logger.critical("Please, check the command line.")
+            self.logger.critical("The module %s don't exist", self.program)
 
     def fakeapp(self):
-        fake_app_path = self.find_fake_app()
+        try:
+            STFA = STFakeApp()
+            if STFA.is_steam_running():
+                self.fake_app = STFA.run_wrapper(self.cParams.cli[1])
+            else:
+                self.logger.critical("Unable to locate a running instance of steam.")
+                self.logger.critical("Please, start the Steam and try again.")
+                sys.exit(1)
+        except IndexError:
+            self.logger.critical("Unable to locate the gameID.")
+            self.logger.critical("Please, check the command line.")
+            sys.exit(1)
 
         try:
-            fake_app_exec = [fake_app_path, self.cParams.cli[1]]
-        except IndexError:
-            return 1
-
-        if fake_app_path[-3:] != 'exe':
-            fake_app_exec = ['python']+fake_app_exec
-
-        self.fake_app = Popen(fake_app_exec, stdout=PIPE, stderr=PIPE)
-        self.fake_app_stderr = self.fake_app.communicate()[1]
-
-        return 0
+            ret = self.fake_app.wait()
+            sys.exit(ret)
+        except KeyboardInterrupt:
+            pass
---
+++ stlib/stfakeapp.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+#
+# Lara Maia <dev@lara.click> 2015 ~ 2016
+#
+# The Steam Tools is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The Steam Tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+
+import atexit
+import locale
+import os
+import sys
+from ctypes import CDLL
+from logging import getLogger
+from subprocess import Popen, PIPE
+
+if os.name is 'posix':
+    import site
+
+
+class STFakeApp:
+    def __init__(self):
+        atexit.register(self._safe_exit)
+        self.logger = getLogger('root')
+        try:
+            self.libsteam_path = self._find_libsteam()
+            self.steam_api = CDLL(self.libsteam_path)
+        except OSError:
+            self.logger.critical("Something is wrong with your installation.")
+            self.logger.critical("I cannot found some libraries needed for steam-tools work")
+            return 1
+
+    def _safe_exit(self, SIG=None, FRM=None):
+        self.logger.warning('Exiting...')
+        if 'wrapper_process' in dir(self):
+            self.wrapper_process.terminate()
+            self.logger.debug("Waiting to libsteam_wrapper terminate.")
+            self.wrapper_process.wait()
+            if self.wrapper_process.returncode:
+                error = self.wrapper_process.stderr.read()
+                self.logger.critical(error.decode(locale.getpreferredencoding()))
+                return 1
+            else:
+                return 0
+
+    def _find_libsteam(self):
+        if os.name == 'nt':
+            ext = '.dll'
+        else:
+            ext = '.so'
+
+        if sys.maxsize > 2 ** 32:
+            libsteam_path = os.path.join('lib64', 'libsteam_api' + ext)
+        else:
+            libsteam_path = os.path.join('lib32', 'libsteam_api' + ext)
+
+        if not os.path.isfile(libsteam_path):
+            if os.name == 'nt':
+                return False
+            else:
+                lib_dir = 'share/steam-tools'
+                if os.path.isfile(os.path.join('/usr/local', lib_dir, libsteam_path)):
+                    STEAM_API = os.path.join('/usr/local', lib_dir, libsteam_path)
+                elif os.path.isfile(os.path.join('/usr', lib_dir, libsteam_path)):
+                    STEAM_API = os.path.join('/usr', lib_dir, libsteam_path)
+                else:
+                    return False
+
+        return libsteam_path
+
+    def _find_wrapper(self):
+        paths = []
+        paths.append(os.path.dirname(os.path.abspath(sys.argv[0])))
+        paths.append(os.path.join(paths[0], 'stlib'))
+
+        if os.name is 'posix':
+            paths.append(os.path.join(site.getsitepackages()[1], 'stlib'))
+
+        for path in paths:
+            for ext in ['', '.exe', '.py']:
+                full_path = os.path.join(path, 'libsteam_wrapper' + ext)
+
+                if os.path.isfile(full_path):
+                    return full_path
+
+        self.logger.critical("Unable to locate some libraries and files.")
+        self.logger.critical("Please, verify your installation.")
+        sys.exit(1)
+
+    def is_steam_running(self):
+        if self.steam_api.SteamAPI_IsSteamRunning():
+            return True
+        else:
+            return False
+
+    def run_wrapper(self, app_id):
+        wrapper_path = self._find_wrapper()
+        wrapper_exec = [wrapper_path, app_id, self.libsteam_path]
+
+        if wrapper_path[-3:] != 'exe':
+            wrapper_exec = ['python'] + wrapper_exec
+
+        self.wrapper_process = Popen(wrapper_exec, stdout=PIPE, stderr=PIPE)
+
+        return self.wrapper_process
