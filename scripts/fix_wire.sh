e2b086a9788624938a2d429edfbec67d66b620f4
Lara Maia <dev@lara.click>
2016-10-13 19:07:01 -0300

    [scripts] Update fix_wire

--- scripts/fix_wire.sh
+++ scripts/fix_wire.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed 's/0.019999999552965164/0.02/g' -i ../ui/interface.xml
-sed '/Steam login status/{n;s|pixbuf">steam|pixbuf">icons/steam|}' -i ../ui/interface.xml
-sed '/SteamGifts login status/{n;s|pixbuf">steam|pixbuf">icons/steamgifts|}' -i ../ui/interface.xml
-sed '/SteamCompanion login status/{n;s|pixbuf">steam|pixbuf">icons/steamcompanion|}' -i ../ui/interface.xml
+sed '/Steam login status/{n;s|pixbuf">icons/steam-tools|pixbuf">icons/steam|}' -i ../ui/interface.xml
+sed '/SteamGifts login status/{n;s|pixbuf">icons/steam-tools|pixbuf">icons/steamgifts|}' -i ../ui/interface.xml
+sed '/SteamCompanion login status/{n;s|pixbuf">icons/steam-tools|pixbuf">icons/steamcompanion|}' -i ../ui/interface.xml
+sed 's|icon">icons/steam-tools.png|icon">icons/steam-tools.ico|' -i ../ui/interface.xml
