575dffba7dc26e08659398a91fd3c2f117af228d
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-11-20 01:33:34 -0800

    Update release.py

--- release.py
+++ release.py
@@ -117,8 +117,10 @@ def build(current_system, current_arch):
 #            params = ['Python34']
 #            print (os.environ['PYTHONPATH'])
 
-        params = [os.environ['PYTHONPATH']]
+        params = [os.environ['PYTHONPATH']]    
         print(">>>>>>>>>> params: ",params)
+        params = params[3:]
+        print(">>>>>>>>>> params2: ",params)
         
         archive_extension_ = '.zip'
 
